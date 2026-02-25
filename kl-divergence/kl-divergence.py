77e4cbfa5aca20b090342c1df1eafde8023dafe6
Gurban_Burjuzada <burjuzada@gmail.com>
2026-02-25 22:32:06 +0400

    Update solution for kl-divergence

--- kl-divergence/kl-divergence.py
+++ kl-divergence/kl-divergence.py
@@ -4,12 +4,11 @@ def kl_divergence(p, q, eps=1e-12):
     """
     Compute KL Divergence D_KL(P || Q).
     """
-    p = np.array(p)
-    q = np.array(q)
+    p = np.array(p, dtype=float)
+    q = np.array(q, dtype=float)
     q = q + eps
-    D_KL = 0
-    for i in range(len(p)):
-        if p[i] == 0:
-            p[i] += eps
-        D_KL += (p[i] * np.log(p[i] / q[i]))
+    p /= np.sum(p)
+    q /= np.sum(q)
+    # Handle the case when p[i] = 0 by assigning 0 to them
+    D_KL = np.sum(np.where(p != 0, p * np.log(p / q), 0))
     return D_KL
