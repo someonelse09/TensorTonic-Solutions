de48b74754100d9f6775ff1a3bb785fdd7e6e6b8
Gurban_Burjuzada <burjuzada@gmail.com>
2026-03-18 20:21:41 +0400

    Update solution for transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -7,7 +7,7 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
     PE = np.zeros((seq_length, d_model))
     for pos in range(seq_length):
         for i in range(d_model):
-            if (i % 2 == 1):
+            if i % 2 == 1:
                 PE[pos, i] += np.cos(pos / (10 ** (8 * i / d_model)))
             else:
                 PE[pos, i] += np.sin(pos / (10 ** (8 * i / d_model)))
