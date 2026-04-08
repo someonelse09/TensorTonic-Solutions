a821c69b8ee3f13aa5484782995c042b9752631d
Gurban_Burjuzada <burjuzada@gmail.com>
2026-04-08 15:34:21 +0400

    Update solution for transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -6,7 +6,14 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     """
     Compute scaled dot-product attention.
     """
+    #  softmax(Q @ K.T / math.sqrt(d_k)) @ V
     d_k = Q.shape[2]
-    #  (Q @ K.T / math.sqrt(d_k)) @ V
-    return F.scaled_dot_product_attention(Q, K, V)
-    
+    # Computing Scores tensor
+    # We transpose the last two dims (-2 and -1) of K to align them for multiplication
+    scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
+
+    # Applying Softmax to scores
+    # This turns scores into values between 0 and 1 that sum to 1
+    weights = F.softmax(scores, dim=-1)
+
+    return torch.matmul(weights, V)
